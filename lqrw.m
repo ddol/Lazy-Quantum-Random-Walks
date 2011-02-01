446aed5d8d1726603d3d1a80ea6970da5ebc7e62
David Dolphin <david@ian.ie>
2011-02-01 19:01:30 +0000

    First 100 itterations of cycles up to length 10

--- lqrw.m
+++ lqrw.m
@@ -5,7 +5,7 @@
 
 
 BeginPackage["lqrw`"];
-SingleLazyItteration::useage = "SingleLazyItteration[State,Coin] returns the\
+SingleLazyItteration::useage = "SingleLazyItteration[State, Coin] returns the\
 state after it has been randomised by one itteration of a given three state coin."
 
 CumulativeMean::usage = "CumulativeMean[List] returns a list where each \
@@ -13,6 +13,15 @@ value is the cumilative mean of all previous list values.";
 
 StatesToPositionalProbabilites::useage = "StatesToPositionalProbabilites[State]\
 returns the position probabilites from a matrix of states.";
+
+MultipleLazySteps::useage = "MultipleLazySteps[State, Coin, Steps] returns the state\
+after a number of itterations using the Coin has been performed."
+
+MultipleLazyStepsHistory::useage = "MultipleLazyStepsHistory is identical \
+to MultipleLazySteps but returns all steps instead of just the final result"
+
+MultipleLazyStepsRecursive::useage = "MultipleLazyStepsRecursive is functionally\
+identical to MultipleLazySteps, but uses a recursive function."
 
 
 Begin["`Private`"];
@@ -37,6 +46,34 @@ StatesToPositionalProbabilites[State0_] := Module[{State=State0},
 CumulativeMean[List0_] := Module[{List = List0}, 
    Return[Accumulate[List]/Range[Length[List]]]];
 
+MultipleLazyStepsRecursive[State0_,Coin0_,Steps0_] := Module[
+{State = State0, Coin = Coin0, Steps = Steps0},
+  Return[Simplify[
+    Nest[
+      (SingleLazyItteration[#,Coin])&,
+      State,Steps]]]]
+
+MultipleLazySteps[State0_,Coin0_,Steps0_] := Module[
+{State = State0, Coin = Coin0, Steps = Steps0},
+  inState = State;
+  For[Step=1,Step<=Steps,Step++,
+    outState = Simplify[
+      SingleLazyItteration[inState, Coin]];
+    inState = outState;];
+  Return[outState];
+]
+
+MultipleLazyStepsHistory[State0_,Coin0_,Steps0_] := Module[
+{State = State0, Coin = Coin0, Steps = Steps0},
+  returnHistory = List[];
+  inState = State;
+  For[Step=1,Step<=Steps,Step++,
+    outState = Simplify[
+      SingleLazyItteration[inState, Coin]];
+    AppendTo[returnHistory, outState];
+    inState = outState;];
+  Return[returnHistory];
+]
 
 End[];
 EndPackage[];
